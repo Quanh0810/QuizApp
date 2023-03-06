340c6793d129df378aaace6c5275d683f5bde7dd
Quanh0810 <nqunhanh35@gmail.com>
2023-03-06 11:24:39 +0700

    load

---
+++ src/Components/Home/home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './home.css'
+
+const Home = () => {
+  return (
+    <section className='home'>
+      <div className="secContainer container">
+        <div className="homeText">
+
+          <h1 className="title">
+            Plan Your Trip With Dot
+          </h1>
+
+          <p className="subTitle">
+            qqqqqqqqqqqqqq
+          </p>
+
+          <button className="btn">
+            <a href="#">Explore Now</a>
+          </button>
+
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
